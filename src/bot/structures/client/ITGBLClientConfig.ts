ce0fdfd5fc62143646cddf75d3ebf5f825ee6eef
Darian Tavana <datavana@gmail.com>
2019-10-18 16:34:46 -0400

    Add mod role id to config interface

--- src/bot/structures/client/ITGBLClientConfig.ts
+++ src/bot/structures/client/ITGBLClientConfig.ts
@@ -4,6 +4,7 @@ interface ITGBLClientConfig {
     PG_CONNECTION: string;
     OWNERS: string[];
     FOOTER_TEXT: string;
+    MOD_ROLE_ID: string;
     API_PORT: number;
     API_SUPER_TOKEN: string;
 }
